95533f3ca6882314f223bd393b62d35e5e458ab2
Dilli Ganesh A <ganeshdilli036@gmail.com>
2025-06-22 22:01:04 +0530

    Add files via upload

---
+++ validation form/validation form/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim username As String
+        Dim Pass As String
+        username = TextBox3.Text
+        Pass = TextBox4.Text
+        If (username.Equals("admin") And Pass.Equals("admin")) Then
+            MessageBox.Show("Success", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Errror", "Information", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+End Class
